896fda7589760ef6504de26650a2d281c401bd48
Raphaël de Courville <raphael.de.courville@gmail.com>
2019-01-02 17:39:47 +0100

    Added initial sound input example. Works but needs fixing to reproduce all the functionality of Shadertoy.

---
+++ examples/inputSound/data/input_sound.glsl
@@ -0,0 +1,110 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+// From Processing 2.1 and up, this line is optional
+#define PROCESSING_COLOR_SHADER
+
+// if you are using the filter() function, replace the above with
+// #define PROCESSING_TEXTURE_SHADER
+
+// ----------------------
+//  SHADERTOY UNIFORMS  -
+// ----------------------
+
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iTime;                 // shader playback time (in seconds) (replaces iGlobalTime which is now obsolete)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform int       iFrame;                // shader playback frame
+
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform vec4      iDate;                 // (year, month, day, time in seconds)
+
+uniform sampler2D iChannel0;             // the audio data as a 512x2 texture
+
+//uniform float     iChannelTime[2];
+//uniform vec3      iChannelResolution[2];
+
+
+// Channels can be either 2D maps or Cubemaps.
+// Pick the ones you need and uncomment them.
+
+
+// uniform float     iChannelTime[1..4];       // channel playback time (in seconds)
+// uniform vec3      iChannelResolution[1..4]; // channel resolution (in pixels)
+
+/*
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+
+uniform samplerCube iChannel0;
+uniform samplerCube iChannel1;
+uniform samplerCube iChannel2;
+uniform samplerCube iChannel3;
+
+uniform vec3  iChannelResolution[4]; // channel resolution (in pixels)
+
+uniform float iChannelTime[4]; // Channel playback time (in seconds)
+*/
+
+vec4 fragColor;
+vec2 fragCoord = gl_FragCoord.xy;
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord );
+
+void main() {
+    mainImage(gl_FragColor,gl_FragCoord.xy);
+}
+
+// ------------------------------
+//  SHADERTOY CODE BEGINS HERE  -
+// ------------------------------
+
+
+// Created by inigo quilez - iq/2013
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+
+// See also:
+//
+// Input - Keyboard    : https://www.shadertoy.com/view/lsXGzf
+// Input - Microphone  : https://www.shadertoy.com/view/llSGDh
+// Input - Mouse       : https://www.shadertoy.com/view/Mss3zH
+// Input - Sound       : https://www.shadertoy.com/view/Xds3Rr
+// Input - SoundCloud  : https://www.shadertoy.com/view/MsdGzn
+// Input - Time        : https://www.shadertoy.com/view/lsXGz8
+// Input - TimeDelta   : https://www.shadertoy.com/view/lsKGWV
+// Inout - 3D Texture  : https://www.shadertoy.com/view/4llcR4
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // create pixel coordinates
+	vec2 uv = fragCoord.xy / iResolution.xy;
+
+    // the sound texture is 512x2
+    int tx = int(uv.x*512.0);
+
+	// first row is frequency data (48Khz/4 in 512 texels, meaning 23 Hz per texel)
+	float fft  = texelFetch( iChannel0, ivec2(tx,0), 0 ).x;
+
+    // second row is the sound wave, one texel is one mono sample
+    float wave = texelFetch( iChannel0, ivec2(tx,1), 0 ).x;
+
+	// convert frequency to colors
+	vec3 col = vec3( fft, 4.0*fft*(1.0-fft), 1.0-fft ) * fft;
+
+    // add wave form on top
+	col += 1.0 -  smoothstep( 0.0, 0.15, abs(wave - uv.y) );
+
+	// output final color
+	fragColor = vec4(col,1.0);
+}
+
+
+// ----------------------------
+//  SHADERTOY CODE ENDS HERE  -
+// ----------------------------
---
+++ examples/inputSound/inputSound.pde
@@ -0,0 +1,119 @@
+// https://www.shadertoy.com/view/Xds3Rr
+
+import processing.sound.*;
+
+SoundFile soundfile;
+FFT fft;
+AudioDevice device;
+
+PGraphics audioDataTexture;
+
+// Define how many FFT bands we want
+int bands = 512;
+
+PShader myShader;
+
+// uniform float     iChannelTime[4];       // channel playback time (in seconds)
+// uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+
+// uniform samplerXX iChannel0..3;          // input channel. XX = 2D/Cube
+
+float previousTime = 0.0;
+
+boolean mouseDragged = false;
+
+PVector lastMousePosition;
+float mouseClickState = 0.0;
+
+void setup() {
+  size(640, 360, P2D);
+  
+  audioDataTexture = createGraphics(512,2,P2D);
+
+  //Load a soundfile
+  soundfile = new SoundFile(this, "beat.aiff");
+
+  // If the Buffersize is larger than the FFT Size, the FFT will fail
+  // so we set Buffersize equal to bands
+  device = new AudioDevice(this, 23000, bands);
+
+  // Load the shader file from the "data" folder
+  myShader = loadShader("input_sound.glsl");
+
+  // We assume the dimension of the window will not change over time,
+  // therefore we can pass its values in the setup() function
+  myShader.set("iResolution", float(width), float(height), 0.0);
+
+  lastMousePosition = new PVector(float(mouseX),float(mouseY));
+
+  // Play the file in a loop
+  soundfile.loop();
+  
+  // Create and patch the FFT analyzer
+  fft = new FFT(this, bands);
+  fft.input(soundfile);
+}
+
+
+void draw() {
+  
+  fft.analyze();
+  
+  audioDataTexture.beginDraw();
+  audioDataTexture.colorMode(RGB,1.0);
+  audioDataTexture.noFill();
+  for (int i = 0; i < bands; i++) {
+    for (int j = 0; j < 2; j++) {
+      float colorValueFromAudio = fft.spectrum[i];
+      audioDataTexture.stroke(colorValueFromAudio);
+      audioDataTexture.pushMatrix();
+      audioDataTexture.translate(i,0);
+      audioDataTexture.line(0,0,0,1);
+      audioDataTexture.popMatrix();
+    }
+  }
+  audioDataTexture.endDraw();
+  
+  myShader.set("iChannel0", audioDataTexture);
+
+  // shader playback time (in seconds)
+  float currentTime = millis()/1000.0;
+  myShader.set("iTime", currentTime);
+
+  // render time (in seconds)
+  float timeDelta = currentTime - previousTime;
+  previousTime = currentTime;
+  myShader.set("iDeltaTime", timeDelta);
+
+  // shader playback frame
+  myShader.set("iFrame", frameCount);
+
+  // mouse pixel coords. xy: current (if MLB down), zw: click
+  if(mousePressed) {
+    lastMousePosition.set(float(mouseX),float(mouseY));
+    mouseClickState = 1.0;
+  } else {
+    mouseClickState = 0.0;
+  }
+  myShader.set( "iMouse", lastMousePosition.x, lastMousePosition.y, mouseClickState, mouseClickState);
+
+  // Set the date
+  // Note that iDate.y and iDate.z contain month-1 and day-1 respectively,
+  // while x does contain the year (see: https://www.shadertoy.com/view/ldKGRR)
+  float timeInSeconds = hour()*3600 + minute()*60 + second();
+  myShader.set("iDate", year(), month()-1, day()-1, timeInSeconds );
+
+  // This uniform is undocumented so I have no idea what the range is
+  myShader.set("iFrameRate", frameRate);
+
+  // Apply the specified shader to any geometry drawn from this point
+  shader(myShader);
+
+  // Draw the output of the shader onto a rectangle that covers the whole viewport.
+  rect(0, 0, width, height);
+
+  resetShader();
+  
+  //image(audioDataTexture,0,0,width,height);
+
+}
